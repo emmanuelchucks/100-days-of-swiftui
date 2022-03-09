fa696af7dd7bad544656b0e626c6ff7c0d51633a
Emmanuel Chucks <echucks19@gmail.com>
2022-03-09 21:52:19 +0000

    a11y: fix image label for voiceover users

--- GuessThatFlag/GuessThatFlag/ContentView.swift
+++ GuessThatFlag/GuessThatFlag/ContentView.swift
@@ -33,6 +33,20 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var tappedFlag = ""
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .top, endPoint: .bottom)
@@ -67,6 +81,7 @@ struct ContentView: View {
                                     .rotation3DEffect(.degrees(showingScore && tappedFlag == countries[number] ? 360 : 0), axis: (x: 0, y: 1, z: 0))
                                     .opacity(showingScore && tappedFlag != countries[number] ? 0.25 : 1)
                                     .animation(.spring(), value: gameCount)
+                                    .accessibilityLabel(labels[countries[number], default: "Unknown flag."])
                             }
                         }
                     }
