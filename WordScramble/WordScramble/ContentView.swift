8f6af0d63c13df0b14805d4f1f63572ab2f85951
Emmanuel Chucks <echucks19@gmail.com>
2022-03-09 22:01:23 +0000

    a11y: enhance used word hint

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -44,6 +44,9 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 } header: {
                     if !usedWords.isEmpty {
